f10b13ea39e597ce4c33f80795194d6113d7f010
GabriellCabrall <gabrielbielcabral@gmail.com>
2026-04-15 15:40:23 -0300

    Cria navegação

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from "react-native";
+import { useState } from "react";
 import { useFonts, Inter_400Regular } from "@expo-google-fonts/inter";
 import { Imbue_400Regular } from "@expo-google-fonts/imbue";
 
 import { colors } from "./src/constants/colors";
-import { newsMock } from "./src/data/news";
+import { newsMock, NewsItem } from "./src/data/news";
+import { HomeScreen } from "./src/screens/HomeScreen";
 import { NewsDetailScreen } from "./src/screens/NewsDetailScreen";
+
+type Screen = "home" | "detail";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,8 +16,25 @@ export default function App() {
     Imbue: Imbue_400Regular,
   });
 
+  const [currentScreen, setCurrentScreen] = useState<Screen>("home");
+  const [selectedNews, setSelectedNews] = useState<NewsItem | null>(null);
+
   if (!fontsLoaded) {
     return null;
+  }
+
+  function handleOpenNews(id: string) {
+    const news = newsMock.find((item) => item.id === id);
+
+    if (!news) return;
+
+    setSelectedNews(news);
+    setCurrentScreen("detail");
+  }
+
+  function handleBackHome() {
+    setCurrentScreen("home");
+    setSelectedNews(null);
   }
 
   return (
@@ -22,7 +43,12 @@ export default function App() {
         barStyle="light-content"
         backgroundColor={colors.backgroundDark}
       />
-      <NewsDetailScreen item={newsMock[0]} />
+
+      {currentScreen === "home" && <HomeScreen onOpenNews={handleOpenNews} />}
+
+      {currentScreen === "detail" && selectedNews && (
+        <NewsDetailScreen item={selectedNews} onBack={handleBackHome} />
+      )}
     </>
   );
 }
